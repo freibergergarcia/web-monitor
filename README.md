34156b6fb1e072b5101f56e915bfb21a96418f41
Bruno FG <freibergergarcia@gmail.com>
2019-12-15 16:37:05 +1100

    Add some extra functionality to interact with the user's input, all base structure for the simple command line application which will basically hit some web applications to see if they're up or down.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # web-monitor
 Monitoring web base application status
+
+## Getting Started
+
+Just clone the repository in you local machine
+```
+$ git clone https://github.com/freibergergarcia/web-monitor.git
+```
+
+### Installing
+
+To install Go, head to https://golang.org
+
+### Compile and run
+
+To compile:
+```golang
+go build monitor.go
+```
+
+To run:
+```
+./monitor
+```
+
+To compile and run:
+```go
+go run monitor.go
+```
+
+### Output (sample)
+
+```bash
+➜  web-monitor git:(master) ✗ go run monitor.go
+Who's monitoring?
+Bruno
+Hey Bruno choose an option
+1 - Start monitoring
+2 - Show logs
+0 - Exit
+2
+Option chosen: 2
+Showing logs...
+➜  web-monitor git:(master) ✗
+```
--- monitor.go
+++ monitor.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 
+	whoAreYou()
+	chooseAnOption()
+
+	action := readOption()
+
+	switch action {
+	case 1:
+		fmt.Println("Monitoring...")
+	case 2:
+		fmt.Println("Showing logs...")
+	case 0:
+		fmt.Println("Bye!")
+		os.Exit(0)
+	default:
+		fmt.Println("Couldn't find the option", action)
+		os.Exit(-1)
+	}
+}
+
+func whoAreYou() {
 	fmt.Println("Who's monitoring?")
 	var name string
 	fmt.Scan(&name)
-
 	fmt.Println("Hey", name, "choose an option")
+}
+
+func chooseAnOption() {
 	fmt.Println("1 - Start monitoring")
 	fmt.Println("2 - Show logs")
 	fmt.Println("0 - Exit")
+}
 
+func readOption() int {
 	var action int
 	/* another option to read the input, + verbose and + ~complicated */
 	fmt.Scanf("%d", &action)
-
 	fmt.Println("Option chosen:", action)
+
+	return action
 }
